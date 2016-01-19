f7580e34af9fa1f59128024aa02fae1e29c82da6
Igor Ferreira <igor@futureworkshops.com>
2016-01-19 17:17:14 +0000

    Include device model informations

--- Notifiable-iOS/Model/FWTNotifiableDevice.h
+++ Notifiable-iOS/Model/FWTNotifiableDevice.h
@@ -8,6 +8,31 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface FWTNotifiableDevice : NSObject
 
+/** The registered token for this device */
+@property (nonatomic, strong, readonly) NSData *token;
+/** The id of this device on the server */
+@property (nonatomic, strong, readonly) NSNumber *tokenId;
+/** The user associated with this device. If the device is anonymous, the user will be nil */
+@property (nonatomic, strong, readonly, nullable) NSString *user;
+/** The name of the this device on the server */
+@property (nonatomic, strong, readonly, nullable) NSString *name;
+/** Other saved informations about the device */
+@property (nonatomic, strong, readonly, nullable) NSDictionary *information;
+
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)initWithToken:(NSData *)deviceToken
+                      tokenId:(NSNumber *)deviceTokenId;
+
+- (instancetype)initWithToken:(NSData *)deviceToken
+                      tokenId:(NSNumber *)deviceTokenId
+                         user:(NSString * _Nullable)user
+                         name:(NSString * _Nullable)name
+                  information:(NSDictionary * _Nullable)information NS_DESIGNATED_INITIALIZER;
+
 @end
+
+NS_ASSUME_NONNULL_END
--- Notifiable-iOS/Model/FWTNotifiableDevice.m
+++ Notifiable-iOS/Model/FWTNotifiableDevice.m
@@ -8,6 +8,70 @@
 
 #import "FWTNotifiableDevice.h"
 
+NSString * const FWTNotifiableDeviceToken = @"FWTNotifiableDeviceToken";
+NSString * const FWTNotifiableDeviceTokenId = @"FWTNotifiableDeviceTokenId";
+NSString * const FWTNotifiableDeviceUser = @"FWTNotifiableDeviceUser";
+NSString * const FWTNotifiableDeviceName = @"FWTNotifiableDeviceName";
+NSString * const FWTNotifiableDeviceInformation = @"FWTNotifiableDeviceInformation";
+
+@interface FWTNotifiableDevice () <NSSecureCoding>
+
+@end
+
 @implementation FWTNotifiableDevice
+
++ (BOOL)supportsSecureCoding
+{
+    return YES;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    [aCoder encodeObject:self.token forKey:FWTNotifiableDeviceToken];
+    [aCoder encodeObject:self.tokenId forKey:FWTNotifiableDeviceTokenId];
+    [aCoder encodeObject:self.user forKey:FWTNotifiableDeviceUser];
+    [aCoder encodeObject:self.name forKey:FWTNotifiableDeviceName];
+    [aCoder encodeObject:self.information forKey:FWTNotifiableDeviceInformation];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    NSData *token = [aDecoder decodeObjectForKey:FWTNotifiableDeviceToken];
+    NSNumber *tokenId = [aDecoder decodeObjectForKey:FWTNotifiableDeviceTokenId];
+    NSString *user = [aDecoder decodeObjectForKey:FWTNotifiableDeviceUser];
+    NSString *name = [aDecoder decodeObjectForKey:FWTNotifiableDeviceName];
+    NSDictionary *information = [aDecoder decodeObjectForKey:FWTNotifiableDeviceInformation];
+    
+    NSAssert(token != nil && tokenId != nil, @"The encoded object doesn't have all the required informations.");
+    
+    return [self initWithToken:token tokenId:tokenId user:user name:name information:information];
+}
+
+- (instancetype)initWithToken:(NSData *)token
+                      tokenId:(NSNumber *)tokenId
+{
+    return [self initWithToken:token
+                       tokenId:tokenId
+                          user:nil
+                          name:nil
+                   information:nil];
+}
+
+- (instancetype)initWithToken:(NSData *)token
+                      tokenId:(NSNumber *)tokenId
+                         user:(NSString *)user
+                         name:(NSString *)name
+                  information:(NSDictionary *)information
+{
+    self = [super init];
+    if (self) {
+        self->_token = token;
+        self->_tokenId = tokenId;
+        self->_user = user;
+        self->_name = name;
+        self->_information = information;
+    }
+    return self;
+}
 
 @end

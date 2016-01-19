763c6682cdd20bed6c7f5598772035182e481720
Igor Ferreira <igor@futureworkshops.com>
2016-01-19 16:27:07 +0000

    Create device information model

---
+++ Notifiable-iOS/Model/FWTNotifiableDevice.h
@@ -0,0 +1,13 @@
+//
+//  FWTNotifiableDevice.h
+//  Notifiable-iOS
+//
+//  Created by Igor Fereira on 19/01/2016.
+//  Copyright © 2016 Future Workshops. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FWTNotifiableDevice : NSObject
+
+@end
---
+++ Notifiable-iOS/Model/FWTNotifiableDevice.m
@@ -0,0 +1,13 @@
+//
+//  FWTNotifiableDevice.m
+//  Notifiable-iOS
+//
+//  Created by Igor Fereira on 19/01/2016.
+//  Copyright © 2016 Future Workshops. All rights reserved.
+//
+
+#import "FWTNotifiableDevice.h"
+
+@implementation FWTNotifiableDevice
+
+@end

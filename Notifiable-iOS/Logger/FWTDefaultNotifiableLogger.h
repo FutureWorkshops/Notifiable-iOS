c729db911dab3d4f076ba99fa8e176c8484acbda
Igor Ferreira <igor@futureworkshops.com>
2016-01-19 16:08:59 +0000

    Include class logger

---
+++ Notifiable-iOS/Logger/FWTDefaultNotifiableLogger.h
@@ -0,0 +1,14 @@
+//
+//  FWTDefaultNotifiableLogger.h
+//  Notifiable-iOS
+//
+//  Created by Igor Fereira on 19/01/2016.
+//  Copyright © 2016 Future Workshops. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FWTNotifiableLogger.h"
+
+@interface FWTDefaultNotifiableLogger : NSObject <FWTNotifiableLogger>
+
+@end
--- Notifiable-iOS/Logger/FWTNotifiableLogger.h
+++ Notifiable-iOS/Logger/FWTNotifiableLogger.h
@@ -5,13 +5,19 @@
 //  Created by Igor Fereira on 19/01/2016.
 //  Copyright © 2016 Future Workshops. All rights reserved.
 //
-
-#ifndef FWTNotifiableLogger_h
-#define FWTNotifiableLogger_h
+typedef NS_ENUM(NSUInteger, FWTNotifiableLogLevel) {
+    FWTNotifiableLogLevelNone = 0,
+    FWTNotifiableLogLevelError,
+    FWTNotifiableLogLevelInformation,
+};
 
 @protocol FWTNotifiableLogger <NSObject>
 
+@property (nonatomic, assign) FWTNotifiableLogLevel logLevel;
+
+- (void)logError:(NSError *)error;
+
+@optional
+- (void)logMessage:(NSString *)format, ... NS_FORMAT_FUNCTION(1,2);
 
 @end
-
-#endif /* FWTNotifiableLogger_h */

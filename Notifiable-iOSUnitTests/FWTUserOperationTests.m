3667ca3fda92617bc4b4f32c6bebd81747ab8c39
Igor Ferreira <igor@futureworkshops.com>
2016-01-22 18:53:21 +0000

    Include basic user related tests

---
+++ Notifiable-iOSUnitTests/FWTUserOperationTests.m
@@ -0,0 +1,126 @@
+//
+//  FWTUserOperationTests.m
+//  Notifiable-iOS
+//
+//  Created by Igor Fereira on 22/01/2016.
+//  Copyright © 2016 Future Workshops. All rights reserved.
+//
+
+#import "FWTTestCase.h"
+#import "FWTNotifiableDevice.h"
+#import "FWTRequesterManager.h"
+#import "FWTNotifiableManager.h"
+#import "FWTHTTPRequester.h"
+#import <OCMock/OCMock.h>
+
+@interface FWTUserOperationTests : FWTTestCase
+
+@property (nonatomic, strong) id requesterManagerMock;
+@property (nonatomic, strong) id httpRequestMock;
+@property (nonatomic, strong) FWTNotifiableManager *manager;
+@property (nonatomic, strong) NSNumber *deviceTokenId;
+
+@end
+
+@implementation FWTUserOperationTests
+
+- (NSNumber *)deviceTokenId
+{
+    if (self->_deviceTokenId == nil) {
+        self->_deviceTokenId = @42;
+    }
+    return self->_deviceTokenId;
+}
+
+- (FWTNotifiableManager *)manager
+{
+    if (self->_manager == nil) {
+        self->_manager = [[FWTNotifiableManager alloc] initWithUrl:OCMOCK_ANY
+                                                          accessId:OCMOCK_ANY
+                                                      andSecretKey:OCMOCK_ANY];
+    }
+    return self->_manager;
+}
+
+- (void)mockRequester
+{
+    if (self->_requesterManagerMock == nil) {
+        id requesterManagerMock = OCMClassMock([FWTRequesterManager class]);
+        OCMStub([requesterManagerMock alloc]).andReturn(requesterManagerMock);
+        OCMStub([requesterManagerMock initWithRequester:[OCMArg any]]).andReturn(requesterManagerMock);
+        self->_requesterManagerMock = requesterManagerMock;
+    }
+}
+
+- (void)mockHttpRequest
+{
+    if (self->_httpRequestMock == nil) {
+        id httpMock = OCMClassMock([FWTHTTPRequester class]);
+        OCMStub([httpMock alloc]).andReturn(httpMock);
+        OCMStub([httpMock initWithBaseUrl:[OCMArg any] andAuthenticator:[OCMArg any]]).andReturn(httpMock);
+    }
+}
+
+- (void)setUp {
+    [super setUp];
+    [self mockRequester];
+    [self mockHttpRequest];
+}
+
+- (void)tearDown {
+    [self->_requesterManagerMock stopMocking];
+    self->_requesterManagerMock = nil;
+    
+    [self->_httpRequestMock stopMocking];
+    self->_httpRequestMock = nil;
+    
+    [super tearDown];
+}
+
+- (void)testAssociateUserWithAnonymous {
+    [self stubDeviceUpdateResponse:self.deviceTokenId onMock:self.requesterManagerMock];
+    [self registerAnonymousDeviceWithToken:[@"test" dataUsingEncoding:NSUTF8StringEncoding] tokenId:self.deviceTokenId andError:nil onManager:self.manager andRquesterMock:self.requesterManagerMock];
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"associate"];
+    [self.manager associateDeviceToUser:@"user" completionHandler:^(BOOL success, NSError * _Nullable error) {
+        XCTAssertTrue(success);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    [self waitForExpectationsWithTimeout:1 handler:nil];
+    XCTAssertEqualObjects(self.manager.currentDevice.user, @"user");
+}
+
+- (void)testAssociateUserWithPreviousUser {
+    [self stubDeviceUpdateResponse:self.deviceTokenId onMock:self.requesterManagerMock];
+    [self registerDeviceWithToken:[@"test" dataUsingEncoding:NSUTF8StringEncoding] tokenId:self.deviceTokenId error:nil andUserAlias:@"user" onManager:self.manager andRquesterMock:self.requesterManagerMock];
+    
+    XCTAssertEqualObjects(self.manager.currentDevice.user, @"user");
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"associate"];
+    [self.manager associateDeviceToUser:@"test" completionHandler:^(BOOL success, NSError * _Nullable error) {
+        XCTAssertTrue(success);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    [self waitForExpectationsWithTimeout:1 handler:nil];
+    XCTAssertEqualObjects(self.manager.currentDevice.user, @"test");
+    
+}
+
+- (void) testAnonymiseUser {
+    [self stubDeviceRegisterResponse:self.deviceTokenId onMock:self.requesterManagerMock];
+    [self registerDeviceWithToken:[@"test" dataUsingEncoding:NSUTF8StringEncoding] tokenId:self.deviceTokenId error:nil andUserAlias:@"user" onManager:self.manager andRquesterMock:self.requesterManagerMock];
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"associate"];
+    [self.manager anonymiseTokenWithCompletionHandler:^(BOOL success, NSError * _Nullable error) {
+        XCTAssertTrue(success);
+        XCTAssertNil(error);
+        [expectation fulfill];
+    }];
+    [self waitForExpectationsWithTimeout:1 handler:nil];
+    XCTAssertNotNil(self.manager.currentDevice);
+    XCTAssertNil(self.manager.currentDevice.user);
+}
+
+@end

2f336282339a2775a0bba1b5b852ea3058462028
Igor Ferreira <igor@futureworkshops.com>
2018-09-14 15:57:52 +0200

    Create notification service target

---
+++ Sample/NotificationService/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  NotificationService
+//
+//  Created by Igor Fereira on 14/09/2018.
+//  Copyright © 2018 Future Workshops. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end

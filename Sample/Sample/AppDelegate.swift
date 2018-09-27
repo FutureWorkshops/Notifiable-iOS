bd56ab8396d52e45d97ad5e80b64a49b8d3a5334
Igor Ferreira <igor@futureworkshops.com>
2018-09-27 17:52:34 +0200

    Include fetch request

--- Sample/Sample/AppDelegate.swift
+++ Sample/Sample/AppDelegate.swift
@@ -20,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
+        application.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
+        
         NotifiableManager.syncronizeData(withGroupId: kAppGroupId)
         
         if let serverURL = URL(string: "https://notifiable.futureworkshops.com/") {
